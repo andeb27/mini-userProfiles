f3ad2d0444d2d854595d370ac1381991783f8e4e
Andrew Bowers <andeb27@gmail.com>
2017-01-18 12:12:14 -0700

    user profiles complete with api

--- controller.js
+++ controller.js
@@ -1,5 +1,13 @@
-angular.module('userProfiles').controller('MainController', function($scope){
+angular.module('userProfiles').controller('MainController', function($scope, mainService){
     
-    $scope.thisAppIsBroken = "This angular app is working";
+   $scope.getUsers = function() {
+       mainService.getUsers().then(function(response) {
+           $scope.users = response.data.data;
+       });
+   }
+
+   $scope.getUsers();
+
+   $scope.toggleFavorite = mainService.toggleFavorite;
    
 })
---
+++ mainService.js
@@ -0,0 +1,15 @@
+angular.module('userProfiles')
+.service('mainService', function($http) {
+
+    this.getUsers = function() {
+        return $http({
+            method: 'GET',
+            url: 'https://reqres.in/api/users?page=1'
+        });
+    }
+
+    this.toggleFavorite = function(userIndex) {
+        data[userIndex].isFavorite = !data[userIndex].isFavorite;
+    }
+
+})
